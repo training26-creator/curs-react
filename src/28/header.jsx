566ca7815499eab6924ad40fdba94543e1c589f3
Shtefanika Vadym <vadym.shtefanika.assist@natterbox.com>
2026-01-24 12:15:26 +0200

    sesiunea 28

--- src/28/header.jsx
+++ src/28/header.jsx
@@ -7,10 +7,6 @@
 // !IMPORTANT - 1 singur fisier jsx tine o singura componenta
 // 2. numele componentei sa se reflecte in numele fisierului
 // 3. toate componentele react trebuie sa inceapa cu litera mare
-export function addTwo(a, b) {
-  return a + b;
-}
-
 import { HelloText } from "./hello-text";
 
 // in html ce este = "" este text sau string
@@ -21,10 +17,6 @@ import { HelloText } from "./hello-text";
 // obiec.proprietate
 export function Header({ userName }) {
     // 1. Parte unde putem face magie cu JS
-    function buna() {
-        console.log('buna ziua', userName)
-    }
-
     const userNameUpdated = userName + ' Familia'
     // 2. Este in return unde avem HTML | HTML + JS
   return <HelloText userName={userNameUpdated} />;
